15e3f6153d4c6fff6a8d7c0a7732bcdd94d4c548
ChethanGowda s <130729042+csgoowda@users.noreply.github.com>
2026-07-27 08:16:22 +0530

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -17,13 +17,12 @@ for col in data.columns:
     if data[col].dtype != 'object':
         print(col)
 
-"""
-WEKA Steps
-1. Open WEKA Explorer.
-2. Click Open File.
-3. Load credit-g.arff.
-4. Go to the Preprocess tab.
-5. WEKA lists all attributes.
-6. Attributes with Type = Nominal are categorical.
-7. Attributes with Type = Numeric are real-valued.
-"""
+
+# WEKA Steps
+# 1. Open WEKA Explorer.
+# 2. Click Open File.
+# 3. Load credit-g.arff.
+# 4. Go to the Preprocess tab.
+# 5. WEKA lists all attributes.
+# 6. Attributes with Type = Nominal are categorical.
+# 7. Attributes with Type = Numeric are real-valued.
